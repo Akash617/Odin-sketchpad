8b2fb9ed0e0e78eec5db60e6f3f38357d14a2209
Akash617 <akash617@yahoo.com>
2021-09-21 01:39:35 -0400

    Squares turn black after mouse over, limit on squares

--- js/script.js
+++ js/script.js
@@ -10,6 +10,18 @@ function createGrid(squareNum) {
     }
 }
 
-let squares = getComputedStyle(document.documentElement).getPropertyValue('--squares');
+let squares = 0;
 
-createGrid(prompt("How many sqaures per side?"));
+while (squares <= 0 || squares > 50 ) {
+    squares = prompt(`How many sqaures per side? (1-50)`);
+}
+
+createGrid(squares);
+
+const boxes = document.querySelectorAll('.grid-item');
+
+boxes.forEach((box) => {
+  box.addEventListener('mouseenter', function(event) {
+    event.target.style.backgroundColor = "black";
+  });
+});
